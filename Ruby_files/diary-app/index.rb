ee17eabb1eb4c19f5ada61f3a1554879f01578bc
Jubilee Bredenberg <jubileebredenberg@gmail.com>
2015-10-30 11:45:31 -0400

    updated app with redirects

--- Ruby_files/diary-app/index.rb
+++ Ruby_files/diary-app/index.rb
@@ -16,38 +16,80 @@ end
 def get_message(diary_path_num)
     case diary_path_num
         when 1
-        message = "On this date combination you #{diary_path_num}:\nYou started your first film class."
+        message = "On this date combination of #{diary_path_num}:\nYou started your first film class."
         when 2
-        message = "On this date combination you #{diary_path_num}:\nYour film premiered at the 41st Ann Arbor Film Festival."
+        message = "On this date combination of #{diary_path_num}:\nYour film premiered at the 41st Ann Arbor Film Festival."
         when 3
-        message = "On this date combination you #{diary_path_num}:\nYou moved to NYC."
+        message = "On this date combination of #{diary_path_num}:\nYou moved to NYC."
         when 4
-        message = "On this date combination you #{diary_path_num}:\nYou got hired at Betsey Johnson."
+        message = "On this date combination of #{diary_path_num}:\nYou got hired at Betsey Johnson."
         when 5
-        message = "On this date combination you #{diary_path_num}:\nYou met your future husband."
+        message = "On this date combination of #{diary_path_num}:\nYou met your future husband."
         when 6
-        message = "On this date combination you #{diary_path_num}:\nYou moved to Raleigh NC."
+        message = "On this date combination of #{diary_path_num}:\nYou moved to Raleigh NC."
         when 7
-        message = "On this date combination you #{diary_path_num}:\nYou graduated from high shool."
+        message = "On this date combination of #{diary_path_num}:\nYou graduated from high shool."
         when 8
-        message = "On this date combination you #{diary_path_num}:\nYou made your big sale to Modcloth."
+        message = "On this date combination of #{diary_path_num}:\nYou made your big sale to Modcloth."
         when 9
-        message = "On this date combination you #{diary_path_num}:\nYou became manager of Betsey Johnson's flagship Soho store."
+        message = "On this date combination of #{diary_path_num}:\nYou became manager of Betsey Johnson's flagship Soho store."
         else
         message = "Uh oh! This is impoooossssible, waaaahhhh!"
     end
 end
 
-get '/newpage' do
-    erb :"newpage"
+get '/:diarydate' do
+    setup_index_view
+end 
+
+get '/message/:diary_path_num' do
+    diary_path_num = params[:diary_path_num].to_i
+    @message = get_message(diary_path_num)
+    erb :index
 end
 
-get '/:diarydate' do
+get '/' do
+    erb :form 
+end
+
+post '/' do
+    setup_index_view
+    diarydate = params[:diarydate].gsub("-","")
+    if valid_diarydate(diarydate)
+        diary_path_num = get_diary_path_num(diarydate)
+        redirect "/message/#{diary_path_num}"
+    else
+        @error = "Oops! Try entering your birthdate in the form of mmddyyyy. Try again!"
+        erb :form   
+    end
+end
+end
+
+def setup_index_view
     diarydate = params[:diarydate]
     diary_path_num = get_diary_path_num(diarydate)
     @message = get_message(diary_path_num)
     erb :index
 end
+
+def valid_diarydate(input)
+    if(input.length == 8 && !input.match(/^[0-9]+[0-9]$/).nil?)
+        true
+    else
+        false
+    end
+
+end
+
+
+
+
+
+
+
+
+
+
 
 
 
